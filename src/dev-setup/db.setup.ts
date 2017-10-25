e3150c1a7bb54a6551cc4375d0363d81cc5f8288
danny_pule <danny.p@ri-team.com>
2017-10-25 15:49:38 +0100

    add product data

--- src/dev-setup/db.setup.ts
+++ src/dev-setup/db.setup.ts
@@ -1,5 +1,8 @@
 import * as moment from 'moment';
+import * as faker from 'faker';
 import db from '../models';
+
+// console.log(faker.commerce.productName());
 
 /////////////////////////// WARNING ///////////////////////////
 //// This script will create new tables and insert demo data
@@ -15,11 +18,13 @@ db.Users
   .sync({ force: true })
   .then(() => {
     console.log('Users table created.');
+  })
+  .then(() => {
     // Table created, now create a user
     return db.Users.create({
       FirstName: 'Super',
       LastName: 'Admin',
-      Email: 'Johan.Jones@fake-email.infoz',
+      Email: 'Super.Admin@fake-email.infoz',
       Username: 'superadmin',
       Password: '$2a$07$IcYHfXSjnMBS0M9BBEL/6ejBYCpZh7n6Q7Yw3ujSW9TR4pRz0l1.q', // login with the password `supersecure`
       DateCreated: moment().toISOString(),
@@ -28,7 +33,20 @@ db.Users
     });
   })
   .then(() => {
-    console.log('Demo user inserted into Users table.');
+    // Table created, now create a user
+    return db.Users.create({
+      FirstName: 'Regular',
+      LastName: 'Admin',
+      Email: 'Admin.Admin@fake-email.infoz',
+      Username: 'admin',
+      Password: '$2a$07$IcYHfXSjnMBS0M9BBEL/6ejBYCpZh7n6Q7Yw3ujSW9TR4pRz0l1.q', // login with the password `supersecure`
+      DateCreated: moment().toISOString(),
+      DateUpdated: moment().toISOString(),
+      RoleID: 4, // admin role
+    });
+  })
+  .then(() => {
+    console.log('Demo users inserted into Users table.');
   })
   .catch((err: any) => console.log('Unable to perform action ', err));
 
@@ -37,8 +55,8 @@ db.Users
 // force: true will drop the table if it already exists
 // =================================================
 db.UserRoles
-  // .sync() // or use sync({ force: true }) (this should be used with caution)
-  .sync({ force: true })
+  .sync() // or use sync({ force: true }) (this should be used with caution)
+  // .sync({ force: true })
   .then((data: any) => {
     console.log('UserRoles table created.');
     // Table created, now create the roles
@@ -76,9 +94,48 @@ db.UserRoles
 // force: true will drop the table if it already exists
 // =================================================
 db.UserTokens
+  .sync() // or use sync({ force: true }) (this should be used with caution)
+  // .sync({ force: true })
+  .then(() => {
+    console.log('UserTokens table created.');
+  })
+  .catch((err: any) => console.log('Unable to perform action ', err));
+
+/////////////////////////// WARNING ///////////////////////////
+//// This script will create new tables and insert demo data
+//// Using `force: true` will nuke any existing tables so use with extreme caution
+///////////////////////////////////////////////////////////////
+
+// ==================================================================
+// Create Sales table
+// force: true will drop the table if it already exists
+// =================================================
+db.Sales
   // .sync() // or use sync({ force: true }) (this should be used with caution)
   .sync({ force: true })
   .then(() => {
-    console.log('UserTokens table created.');
+    console.log('Sales table created.');
+  })
+  .then(() => {
+    // Table created, now create sales items
+    // create 30 sales items
+    let companyName: string;
+    for (let i = 0; i < 5; i++) {
+      companyName = faker.company.companyName();
+
+      for (let f = 0; f < 6; f++) {
+        db.Sales.create({
+          SaleDate: moment(faker.date.recent()).toISOString(),
+          CompanyName: companyName,
+          ProductName: faker.commerce.productName(),
+          ProductSKU: faker.random.number({ min: 1234, max: 9876 }),
+          SalesValue: faker.finance.amount(100, 2000, 2),
+          SalesCount: faker.random.number({ min: 12, max: 98 }),
+        });
+      }
+    }
+  })
+  .then(() => {
+    console.log('Demo sales items inserted into Sales table.');
   })
   .catch((err: any) => console.log('Unable to perform action ', err));
---
+++ src/models/Sales.ts
@@ -0,0 +1,43 @@
+export default (sequelize: any, DataTypes: any) => {
+  const Sales = sequelize.define(
+    'Sales',
+    {
+      SaleID: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      SaleDate: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      CompanyName: {
+        allowNull: false,
+        type: DataTypes.STRING(100),
+      },
+      ProductName: {
+        allowNull: false,
+        type: DataTypes.STRING(100),
+      },
+      ProductSKU: {
+        allowNull: false,
+        type: DataTypes.STRING(20),
+      },
+      SalesValue: {
+        allowNull: false,
+        type: DataTypes.DECIMAL,
+      },
+      SalesCount: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+      },
+    },
+    {
+      timestamps: false,
+      freezeTableName: true,
+    }
+  );
+
+  return Sales;
+};
