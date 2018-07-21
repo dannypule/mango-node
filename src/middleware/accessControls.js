fbc15960aa8f70e0909076b1a228c5f4f66fa8dc
dannypule <kojikusan@gmail.com>
2018-07-21 19:47:06 +0100

    add more access controls

--- src/middleware/accessControls.js
+++ src/middleware/accessControls.js
@@ -1,36 +1,67 @@
 import db from '../db_models';
 import utils from '../utils/utils';
 
-export const appRoles = {
+export const APP_ROLES = {
   SUPERADMIN: 110,
   ADMIN: 100,
   COMPANY_ADMIN: 50,
   COMPANY_EDITOR: 40,
-  COMPANY_VIEWER: 40,
+  COMPANY_VIEWER: 30,
+  COMPANY_REGULAR: 20,
   SELF: 'SELF',
 };
 
-export const users = {
-  accessControls: (allowedUsers) => async (req, res, next) => {
-    const userIdFromRequest = req.body.id;
-    const authenticatedUser = req.user;
+// user can view, update, add his own user data
+// user can only view users from his company
+// user can only update users from his company with company_editor or company_admin permissing
+// user can only add users to his company with company_editor or company_admin permissing
 
-    if (allowedUsers.includes(appRoles.SELF)) {
-      if (userIdFromRequest === authenticatedUser.id) {
+export const users = {
+  accessControls: (allowedRoles) => async (req, res, next) => {
+    const { user } = req;
+
+    if (allowedRoles.includes(APP_ROLES.VIEWER)) {
+      if (user.userRoleCode === APP_ROLES.VIEWER) {
         return next();
       }
     }
-    if (allowedUsers.includes(appRoles.ADMIN)) {
-      if (authenticatedUser.userRoleCode >= appRoles.ADMIN) {
+
+    if (allowedRoles.includes(APP_ROLES.COMPANY_EDITOR)) {
+      if (user.userRoleCode === APP_ROLES.COMPANY_EDITOR) {
         return next();
       }
     }
+
+    if (allowedRoles.includes(APP_ROLES.COMPANY_ADMIN)) {
+      if (user.userRoleCode === APP_ROLES.COMPANY_ADMIN) {
+        return next();
+      }
+    }
+
+    if (user.userRoleCode >= APP_ROLES.ADMIN) {
+      return next();
+    }
+
+    utils.fail(res, { message: 'You are not allowed to perform that action.' }, 403);
+  },
+  allowSelf: () => (req, res, next) => {
+    const { user } = req;
+    const userFromBody = req.user;
+
+    if (user.id === userFromBody.id) {
+      return next();
+    }
+
+    if (user.userRoleCode >= APP_ROLES.ADMIN) {
+      return next();
+    }
+
     utils.fail(res, { message: 'You are not allowed to perform that action.' }, 403);
   },
 };
 
 export const companies = {
-  accessControls: (allowedUsers) => async (req, res, next) => {
+  accessControls: (allowedRoles) => async (req, res, next) => {
     const companyIdFromRequest = req.body.id;
     const authenticatedUser = req.user;
     const result = await db.UserCompany.find({
@@ -40,10 +71,10 @@ export const companies = {
       },
     });
 
-    if (result && allowedUsers.includes(result.access_type)) {
+    if (result && allowedRoles.includes(result.access_type)) {
       return next();
     }
-    if (allowedUsers.includes('ADMIN')) {
+    if (allowedRoles.includes('ADMIN')) {
       if (authenticatedUser.userRoleCode >= 100) {
         return next();
       }
