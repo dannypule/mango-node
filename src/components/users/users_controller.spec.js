d8bed46d9779cee8acb7a0665bbeed68fd175b2b
dannypule <kojikusan@gmail.com>
2018-09-17 22:02:11 +0100

    add int tests

--- src/components/users/users_controller.spec.js
+++ src/components/users/users_controller.spec.js
@@ -1,98 +1,218 @@
-import UsersController from './users_controller';
+import axios from 'axios';
+import config from '../../config';
 
-const user = {
-  id: 12,
-  firstName: 'bono',
-  lastName: 'u2',
-  email: 'bono@u2.com',
-  userRoleCode: 'role-code',
-  companyId: 'company123',
-  createdAt: '123',
-  updatedAt: '321',
-};
-const findAndCountAllMock = jest.fn().mockReturnValue({
-  count: 10,
-  rows: [
-    {
-      id: 12,
-      first_name: user.firstName,
-      last_name: user.lastName,
-      email: user.email,
-      password: 'some-password',
-      company_id: user.companyId,
-      user_role_code: user.userRoleCode,
-      created_at: user.createdAt,
-      updated_at: user.updatedAt,
-    },
-  ],
+const axiosInstance = axios.create({
+  baseURL: `${config.baseURL}`,
+  timeout: 5000,
 });
-const destroyMock = jest.fn().mockReturnValue(1);
-const destroyFailMock = jest.fn().mockReturnValue(0);
-const responseContent = user;
-const responsePagination = {
-  content: [responseContent],
-  count: 10,
-  page: 1,
-  pages: 1,
-  length: 1,
-};
-const successMock = jest.fn();
-const failMock = jest.fn();
-const reqMock = {
-  query: { id: 1, companyId: 2 },
-};
-const resMock = jest.fn();
-const addUserMock = jest.fn();
-let controller;
 
-xdescribe('UsersController', () => {
-  describe('when getUsers is called', () => {
-    beforeEach(() => {
-      jest.clearAllMocks();
-      controller = new UsersController(
-        { findAndCountAll: findAndCountAllMock, destroy: destroyMock },
-        { success: successMock, fail: failMock },
-        { addUser: addUserMock },
-      );
-      controller.getUsers(reqMock, resMock);
+describe('Given /api/users', () => {
+  describe('and a super user is logged in', () => {
+    beforeAll((done) => {
+      axiosInstance
+        .post('/api/auth/login', {
+          email: 'super.admin@email.fake',
+          password: 'supersecure',
+        })
+        .then(res => {
+          axiosInstance.defaults.headers.common.Authorization = res.data.data.token;
+          done();
+        });
     });
-    it('should call successMock', () => {
-      expect(failMock).not.toHaveBeenCalled();
-      expect(successMock).toHaveBeenCalledWith(resMock, responsePagination);
-    });
-    it('should call findAndCountAllMock', () => {
-      expect(findAndCountAllMock).toHaveBeenCalledTimes(1);
-    });
-  });
-  describe('when deleteUser is called', () => {
-    beforeEach(() => {
-      jest.clearAllMocks();
-      controller = new UsersController(
-        { findAndCountAll: findAndCountAllMock, destroy: destroyMock },
-        { success: successMock, fail: failMock },
-        { addUser: addUserMock },
-      );
-      controller.deleteUser({ body: { email: user.email } }, resMock);
-    });
-    it('should call successMock', () => {
-      expect(failMock).not.toHaveBeenCalled();
-      expect(successMock).toHaveBeenCalledWith(resMock);
-    });
-    describe('and user is not found', () => {
-      beforeEach(() => {
-        jest.clearAllMocks();
-        controller = new UsersController(
-          { findAndCountAll: findAndCountAllMock, destroy: destroyFailMock },
-          { success: successMock, fail: failMock },
-          { addUser: addUserMock },
-        );
-        controller.deleteUser({ body: { email: user.email } }, resMock);
-      });
-      it('should call failMock', () => {
-        expect(failMock).toHaveBeenCalledWith(resMock, { message: 'Unable to delete this user.' });
-        expect(failMock).toHaveBeenCalledTimes(1);
-        expect(successMock).not.toHaveBeenCalled();
+
+    /* GET /api/users */
+    describe('/api/users GET', () => {
+      it('should return users', (done) => {
+        axiosInstance
+          .get('/api/users')
+          .then((res) => {
+            expect(res.status).toBe(200);
+            expect(res.data.ok).toBe(true);
+            expect(res.data.data.content.length).toBe(15);
+            expect(res.data.data.page).toBe(1);
+            expect(res.data.data.length).toBe(15);
+            done();
+          })
+          .catch(err => {
+            console.log(err);
+            done();
+            throw new Error(err);
+          });
       });
     });
+
+    /* GET /api/users?page=4 */
+    describe('and page 4 is requested', () => {
+      it('should return users from page 4', (done) => {
+        axiosInstance
+          .get('/api/users?page=4')
+          .then((res) => {
+            expect(res.status).toBe(200);
+            expect(res.data.ok).toBe(true);
+            expect(res.data.data.content.length).toBe(15);
+            expect(res.data.data.page).toBe(4);
+            expect(res.data.data.length).toBe(15);
+            done();
+          })
+          .catch(err => {
+            console.log(err);
+            done();
+            throw new Error(err);
+          });
+      });
+    });
+
+    /* GET /api/users?id=5 */
+    describe('and user with id of 5 is requested', () => {
+      it('should return user with id of 5', (done) => {
+        axiosInstance
+          .get('/api/users?id=5')
+          .then((res) => {
+            expect(res.status).toBe(200);
+            expect(res.data.ok).toBe(true);
+            expect(res.data.data.content.length).toBe(1);
+            expect(res.data.data.page).toBe(1);
+            expect(res.data.data.length).toBe(1);
+            done();
+          })
+          .catch(err => {
+            console.log(err);
+            done();
+            throw new Error(err);
+          });
+      });
+    });
+
+    /* POST /api/users/add_user */
+    describe('and user wants to add user', () => {
+      let newUserId;
+
+      afterAll((done) => {
+        if (newUserId) {
+          const postData = { id: newUserId };
+          console.log(`Deleting user #${newUserId}`);
+          axiosInstance
+            .delete('/api/users/remove_user', {
+              data: postData,
+            })
+            .then((res) => {
+              if (res.data.ok) {
+                console.log(`User #${newUserId} was deleted`);
+              } else {
+                console.log(`Unable to delete user #${newUserId}`);
+              }
+              done();
+            });
+        } else {
+          done();
+        }
+      });
+
+      it('should add user', (done) => {
+        const postData = {
+          firstName: 'some',
+          lastName: 'user',
+          email: 'some.user@email.fakez',
+          password: 'supersecure',
+          companyId: 1,
+        };
+        axiosInstance
+          .post('/api/users/add_user', postData)
+          .then((res) => {
+            expect(res.status).toBe(200);
+            expect(res.data.ok).toBe(true);
+            expect(res.data.data.user).toBeTruthy();
+            expect(res.data.data.token.includes('Bearer')).toBe(false);
+            newUserId = res.data.data.user.id;
+            done();
+          })
+          .catch(err => {
+            console.log(err);
+            done();
+            throw new Error(err);
+          });
+      });
+
+      /* PUT /api/users/update_user */
+      describe('and user wants to update user', () => {
+        it('should update user', (done) => {
+          const postData = {
+            id: newUserId,
+            firstName: 'updatedFirstName',
+            lastName: 'updatedLastName',
+            email: 'updated.email@email.fakez',
+            userRoleCode: 40,
+            status: 'INACTIVE',
+          };
+          axiosInstance
+            .put('/api/users/update_user', postData)
+            .then((res) => {
+              expect(res.status).toBe(200);
+              expect(res.data.ok).toBe(true);
+
+              expect(res.data.data.content.firstName).toBe(postData.firstName);
+              expect(res.data.data.content.lastName).toBe(postData.lastName);
+              expect(res.data.data.content.email).toBe(postData.email);
+              expect(res.data.data.content.userRoleCode).toBe(postData.userRoleCode);
+              expect(res.data.data.content.status).toBe(postData.status);
+              done();
+            })
+            .catch(err => {
+              console.log(err);
+              done();
+              throw new Error(err);
+            });
+        });
+      });
+
+      /* PUT /api/users/update_email */
+      describe('and user wants to update email', () => {
+        it('should update email', (done) => {
+          const postData = {
+            id: newUserId,
+            email: 'email.is.updated@email.fakez',
+          };
+          axiosInstance
+            .put('/api/users/update_email', postData)
+            .then((res) => {
+              expect(res.status).toBe(200);
+              expect(res.data.ok).toBe(true);
+              expect(res.data.data.content.email).toBe(postData.email);
+              done();
+            })
+            .catch(err => {
+              console.log(err);
+              done();
+              throw new Error(err);
+            });
+        });
+      });
+
+      /* PUT /api/users/update_user */
+      describe('and user wants to update user', () => {
+        it('should update name', (done) => {
+          const postData = {
+            id: newUserId,
+            firstName: 'updated_first_name',
+            lastName: 'updated_last_name',
+          };
+          axiosInstance
+            .put('/api/users/update_user', postData)
+            .then((res) => {
+              expect(res.status).toBe(200);
+              expect(res.data.ok).toBe(true);
+              expect(res.data.data.content.firstName).toBe(postData.firstName);
+              expect(res.data.data.content.lastName).toBe(postData.lastName);
+              done();
+            })
+            .catch(err => {
+              console.log(err);
+              done();
+              throw new Error(err);
+            });
+        });
+      });
+    });
   });
 });
+
