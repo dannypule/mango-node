b4ebda49286ba650f55726b11ac0405cde0d7eee
dannypule <kojikusan@gmail.com>
2018-09-17 22:02:11 +0100

    add int tests

--- src/components/auth/auth_controller.spec.js
+++ src/components/auth/auth_controller.spec.js
@@ -7,92 +7,95 @@ const axiosInstance = axios.create({
 });
 
 describe('Given AuthController', () => {
-  beforeAll((done) => {
-    axiosInstance
-      .post('/api/auth/login', {
-        email: 'super.admin@email.fake',
-        password: 'supersecure',
-      })
-      .then(res => {
-        axiosInstance.defaults.headers.common.Authorization = res.data.data.token;
-        done();
-      });
-  });
-
-  /* login */
-  describe('user tries to login', () => {
-    const postData = {
-      email: 'company.admin@email.fake',
-      password: 'supersecure',
-    };
-
-    it('should log user in', (done) => {
+  describe('and a super user is logged in', () => {
+    beforeAll((done) => {
       axiosInstance
-        .post('/api/auth/login', postData)
-        .then((res) => {
-          expect(res.status).toBe(200);
-          expect(res.data.ok).toBe(true);
-          expect(res.data.data.token.includes('Bearer')).toBe(true);
-          done();
+        .post('/api/auth/login', {
+          email: 'super.admin@email.fake',
+          password: 'supersecure',
         })
-        .catch(err => {
-          console.log(err);
-          throw new Error(err);
+        .then(res => {
+          axiosInstance.defaults.headers.common.Authorization = res.data.data.token;
+          done();
         });
     });
-  });
 
-  /* register */
-  describe('and user tries to register an account', () => {
-    let postData;
-    let newUserId;
-
-    beforeEach(() => {
-      postData = {
-        firstName: 'test',
-        lastName: 'user',
-        email: 'test.user@email.fakez',
+    /* /api/auth/login */
+    describe('user tries to login', () => {
+      const postData = {
+        email: 'company.admin@email.fake',
         password: 'supersecure',
-        companyId: 1,
       };
-    });
 
-    afterEach((done) => {
-      if (newUserId) {
-        const postData = { id: newUserId };
-        console.log(`Deleting user #${newUserId}`);
+      it('should log user in', (done) => {
         axiosInstance
-          .delete('/api/users/remove_user', {
-            data: postData,
-          })
+          .post('/api/auth/login', postData)
           .then((res) => {
-            if (res.data.ok) {
-              console.log(`User #${newUserId} deleted`);
-            } else {
-              console.log(`Unable to delete user #${newUserId}`);
-            }
+            expect(res.status).toBe(200);
+            expect(res.data.ok).toBe(true);
+            expect(res.data.data.token.includes('Bearer')).toBe(true);
             done();
+          })
+          .catch(err => {
+            console.log(err);
+            throw new Error(err);
           });
-      } else {
-        done();
-      }
+      });
     });
 
-    it('should register a new user', (done) => {
-      axiosInstance
-        .post('/api/auth/register', postData)
-        .then((res) => {
-          expect(res.status).toBe(200);
-          expect(res.data.ok).toBe(true);
-          expect(res.data.data.user).toBeTruthy();
-          expect(res.data.data.token.includes('Bearer')).toBe(true);
-          newUserId = res.data.data.user.id;
+    /* /api/auth/register */
+    describe('and user tries to register an account', () => {
+      let postData;
+      let newUserId;
+
+      beforeEach(() => {
+        postData = {
+          firstName: 'test',
+          lastName: 'user',
+          email: 'test.user@email.fakez',
+          password: 'supersecure',
+          companyId: 1,
+        };
+      });
+
+      afterEach((done) => {
+        if (newUserId) {
+          const postData = { id: newUserId };
+          console.log(`Deleting user #${newUserId}`);
+          axiosInstance
+            .delete('/api/users/remove_user', {
+              data: postData,
+            })
+            .then((res) => {
+              if (res.data.ok) {
+                console.log(`User #${newUserId} was deleted`);
+              } else {
+                console.log(`Unable to delete user #${newUserId}`);
+              }
+              done();
+            });
+        } else {
           done();
-        })
-        .catch(err => {
-          console.log(err);
-          throw new Error(err);
-        });
+        }
+      });
+
+      it('should register a new user', (done) => {
+        axiosInstance
+          .post('/api/auth/register', postData)
+          .then((res) => {
+            expect(res.status).toBe(200);
+            expect(res.data.ok).toBe(true);
+            expect(res.data.data.user).toBeTruthy();
+            expect(res.data.data.token.includes('Bearer')).toBe(true);
+            newUserId = res.data.data.user.id;
+            done();
+          })
+          .catch(err => {
+            console.log(err);
+            done();
+            throw new Error(err);
+          });
+      });
     });
   });
 });
