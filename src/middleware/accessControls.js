a036ff95dc099090926f8f1583a213bea040f82a
dannypule <kojikusan@gmail.com>
2018-11-18 14:04:31 +0000

    improve access controls

--- src/middleware/accessControls.js
+++ src/middleware/accessControls.js
@@ -1,44 +1,38 @@
+import { getStatusText, FORBIDDEN } from 'http-status-codes';
 import utils from '../utils/utils';
 
-export const SUPERADMIN = 110;
-export const ADMIN = 100;
-export const COMPANY_ADMIN = 50;
-export const COMPANY_EDITOR = 40;
-export const COMPANY_VIEWER = 30;
-export const COMPANY_REGULAR = 20;
 export const SELF = 'SELF';
+export const COMPANY_REGULAR = 20;
+export const COMPANY_VIEWER = 30;
+export const COMPANY_EDITOR = 40;
+export const COMPANY_ADMIN = 50;
+export const ADMIN = 100;
+export const SUPERADMIN = 110;
+
+const authorised = next => next();
+const notAuthorised = res => utils.fail(res, { message: getStatusText(FORBIDDEN) }, FORBIDDEN);
 
 export const access = allowedRoles => async (req, res, next) => {
-  const authUser = req.user;
-  const requestedUser = req.body;
+  const {
+    user: { id, userRoleCode },
+  } = req;
+  const sessionUser = req.body;
 
-  if (allowedRoles.includes(SELF)) {
-    if (requestedUser && requestedUser.id && authUser.id === requestedUser.id) {
-      return next();
-    }
+  if (allowedRoles.includes(SELF) && sessionUser && sessionUser.id && id === sessionUser.id) {
+    return authorised(next);
+  }
+  if (allowedRoles.includes(COMPANY_VIEWER) && userRoleCode === COMPANY_VIEWER) {
+    return authorised(next);
+  }
+  if (allowedRoles.includes(COMPANY_EDITOR) && userRoleCode === COMPANY_EDITOR) {
+    return authorised(next);
+  }
+  if (allowedRoles.includes(COMPANY_ADMIN) && userRoleCode === COMPANY_ADMIN) {
+    return authorised(next);
+  }
+  if (userRoleCode >= ADMIN) {
+    return authorised(next);
   }
 
-  if (allowedRoles.includes(COMPANY_VIEWER)) {
-    if (authUser.userRoleCode === COMPANY_VIEWER) {
-      return next();
-    }
-  }
-
-  if (allowedRoles.includes(COMPANY_EDITOR)) {
-    if (authUser.userRoleCode === COMPANY_EDITOR) {
-      return next();
-    }
-  }
-
-  if (allowedRoles.includes(COMPANY_ADMIN)) {
-    if (authUser.userRoleCode === COMPANY_ADMIN) {
-      return next();
-    }
-  }
-
-  if (authUser.userRoleCode >= ADMIN) {
-    return next();
-  }
-
-  utils.fail(res, { message: 'You are not allowed to perform that action.' }, 403);
+  notAuthorised(res);
 };
